07f96745ce6bd6f275a3fcfd38de822d2a9080d2
Albert Mends <mendsalbert@gmail.com>
2024-03-17 18:08:59 +0000

    cleaning up the dashboard

--- src/component/sidebar/index.jsx
+++ src/component/sidebar/index.jsx
@@ -141,52 +141,6 @@ function Sidebar({ handleActive }) {
                   </div>
                 </Link>
               </li>
-
-              <li className="item py-[11px] text-bgray-900 dark:text-white">
-                <Link href="/history">
-                  <div className="flex items-center justify-between">
-                    <div className="flex items-center space-x-2.5">
-                      <span className="item-ico">
-                        <svg
-                          width="18"
-                          height="21"
-                          viewBox="0 0 18 21"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            d="M17.5 12.5C17.5 17.1944 13.6944 21 9 21C4.30558 21 0.5 17.1944 0.5 12.5C0.5 7.80558 4.30558 4 9 4C13.6944 4 17.5 7.80558 17.5 12.5Z"
-                            fill="#1A202C"
-                            className="path-1"
-                          />
-                          <path
-                            fillRule="evenodd"
-                            clipRule="evenodd"
-                            d="M8.99995 1.75C8.02962 1.75 7.09197 1.88462 6.20407 2.13575C5.80549 2.24849 5.39099 2.01676 5.27826 1.61818C5.16553 1.21961 5.39725 0.805108 5.79583 0.692376C6.81525 0.404046 7.89023 0.25 8.99995 0.25C10.1097 0.25 11.1846 0.404046 12.2041 0.692376C12.6026 0.805108 12.8344 1.21961 12.7216 1.61818C12.6089 2.01676 12.1944 2.24849 11.7958 2.13575C10.9079 1.88462 9.97028 1.75 8.99995 1.75Z"
-                            fill="#22C55E"
-                            className="path-2"
-                          />
-                          <path
-                            d="M11 13C11 14.1046 10.1046 15 9 15C7.89543 15 7 14.1046 7 13C7 11.8954 7.89543 11 9 11C10.1046 11 11 11.8954 11 13Z"
-                            fill="#22C55E"
-                            className="path-2"
-                          />
-                          <path
-                            fillRule="evenodd"
-                            clipRule="evenodd"
-                            d="M9 7.25C9.41421 7.25 9.75 7.58579 9.75 8V12C9.75 12.4142 9.41421 12.75 9 12.75C8.58579 12.75 8.25 12.4142 8.25 12V8C8.25 7.58579 8.58579 7.25 9 7.25Z"
-                            fill="#22C55E"
-                            className="path-2"
-                          />
-                        </svg>
-                      </span>
-                      <span className="item-text text-lg font-medium leading-none">
-                        History
-                      </span>
-                    </div>
-                  </div>
-                </Link>
-              </li>
             </ul>
           </div>
           <div className="item-wrapper mb-5">
