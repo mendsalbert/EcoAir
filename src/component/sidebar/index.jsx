666a64a82f4644ff8a95625820a9859dea73e476
Albert Mends <mendsalbert@gmail.com>
2024-03-17 18:07:51 +0000

    cleaning up the dashboard

--- src/component/sidebar/index.jsx
+++ src/component/sidebar/index.jsx
@@ -143,59 +143,6 @@ function Sidebar({ handleActive }) {
               </li>
 
               <li className="item py-[11px] text-bgray-900 dark:text-white">
-                <Link href="/calender">
-                  <div className="flex items-center justify-between">
-                    <div className="flex items-center space-x-2.5">
-                      <span className="item-ico">
-                        <svg
-                          width="18"
-                          height="21"
-                          viewBox="0 0 18 21"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            d="M0 6.5C0 4.29086 1.79086 2.5 4 2.5H14C16.2091 2.5 18 4.29086 18 6.5V8V17C18 19.2091 16.2091 21 14 21H4C1.79086 21 0 19.2091 0 17V8V6.5Z"
-                            fill="#1A202C"
-                            className="path-1"
-                          />
-                          <path
-                            d="M14 2.5H4C1.79086 2.5 0 4.29086 0 6.5V8H18V6.5C18 4.29086 16.2091 2.5 14 2.5Z"
-                            fill="#22C55E"
-                            className="path-2"
-                          />
-                          <path
-                            fillRule="evenodd"
-                            clipRule="evenodd"
-                            d="M5 0.25C5.41421 0.25 5.75 0.585786 5.75 1V4C5.75 4.41421 5.41421 4.75 5 4.75C4.58579 4.75 4.25 4.41421 4.25 4V1C4.25 0.585786 4.58579 0.25 5 0.25ZM13 0.25C13.4142 0.25 13.75 0.585786 13.75 1V4C13.75 4.41421 13.4142 4.75 13 4.75C12.5858 4.75 12.25 4.41421 12.25 4V1C12.25 0.585786 12.5858 0.25 13 0.25Z"
-                            fill="#1A202C"
-                            className="path-2"
-                          />
-                          <circle cx="9" cy="14" r="1" fill="#22C55E" />
-                          <circle
-                            cx="13"
-                            cy="14"
-                            r="1"
-                            fill="#22C55E"
-                            className="path-2"
-                          />
-                          <circle
-                            cx="5"
-                            cy="14"
-                            r="1"
-                            fill="#22C55E"
-                            className="path-2"
-                          />
-                        </svg>
-                      </span>
-                      <span className="item-text text-lg font-medium leading-none">
-                        Calender
-                      </span>
-                    </div>
-                  </div>
-                </Link>
-              </li>
-              <li className="item py-[11px] text-bgray-900 dark:text-white">
                 <Link href="/history">
                   <div className="flex items-center justify-between">
                     <div className="flex items-center space-x-2.5">
