7cd5cc519889303826ab8a5766b8000810832112
Albert Mends <mendsalbert@gmail.com>
2024-03-17 15:09:05 +0000

    cleaning up the dashboard

--- src/app/(sharedLayout)/page.js
+++ src/app/(sharedLayout)/page.js
@@ -18,7 +18,7 @@ function Home() {
             <Efficiency />
           </div>
 
-          <ListTab />
+          {/* <ListTab /> */}
         </section>
         {/* <section className="flex w-full flex-col space-x-0 lg:flex-row lg:space-x-6 2xl:w-[400px] 2xl:flex-col 2xl:space-x-0">
           <Wallet />
