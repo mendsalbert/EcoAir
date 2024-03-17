c71051357e17c4d7b1c71c18991ca6ae9dc168b1
Albert Mends <mendsalbert@gmail.com>
2024-03-17 11:27:16 +0000

    cleaning up the dashboard

--- src/component/sidebar/index.jsx
+++ src/component/sidebar/index.jsx
@@ -73,35 +73,35 @@ function Sidebar({ handleActive }) {
                   className="item py-[11px] text-bgray-900 dark:text-white"
                   onClick={() => setActiveDashboard(!activeDashboard)}
                 >
-                  <a className="cursor-pointer">
-                    <div className="flex items-center justify-between">
-                      <div className="flex items-center space-x-2.5">
-                        <span className="item-ico">
-                          <svg
-                            width="18"
-                            height="21"
-                            viewBox="0 0 18 21"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              className="path-1"
-                              d="M0 8.84719C0 7.99027 0.366443 7.17426 1.00691 6.60496L6.34255 1.86217C7.85809 0.515019 10.1419 0.515019 11.6575 1.86217L16.9931 6.60496C17.6336 7.17426 18 7.99027 18 8.84719V17C18 19.2091 16.2091 21 14 21H4C1.79086 21 0 19.2091 0 17V8.84719Z"
-                              fill="#1A202C"
-                            />
-                            <path
-                              className="path-2"
-                              d="M5 17C5 14.7909 6.79086 13 9 13C11.2091 13 13 14.7909 13 17V21H5V17Z"
-                              fill="#22C55E"
-                            />
-                          </svg>
-                        </span>
-                        <span className="item-text text-lg font-medium leading-none">
-                          Dashboards
-                        </span>
-                      </div>
+                  {/* <a className="cursor-pointer"> */}
+                  <div className="flex items-center justify-between">
+                    <div className="flex items-center space-x-2.5">
+                      <span className="item-ico">
+                        <svg
+                          width="18"
+                          height="21"
+                          viewBox="0 0 18 21"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            className="path-1"
+                            d="M0 8.84719C0 7.99027 0.366443 7.17426 1.00691 6.60496L6.34255 1.86217C7.85809 0.515019 10.1419 0.515019 11.6575 1.86217L16.9931 6.60496C17.6336 7.17426 18 7.99027 18 8.84719V17C18 19.2091 16.2091 21 14 21H4C1.79086 21 0 19.2091 0 17V8.84719Z"
+                            fill="#1A202C"
+                          />
+                          <path
+                            className="path-2"
+                            d="M5 17C5 14.7909 6.79086 13 9 13C11.2091 13 13 14.7909 13 17V21H5V17Z"
+                            fill="#22C55E"
+                          />
+                        </svg>
+                      </span>
+                      <span className="item-text text-lg font-medium leading-none">
+                        Dashboards
+                      </span>
                     </div>
-                  </a>
+                  </div>
+                  {/* </a> */}
                 </li>
               </Link>
               <li className="item py-[11px] text-bgray-900 dark:text-white">
