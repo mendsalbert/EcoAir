f03001837e828cdf6b62c2393b890a66c47fbc45
Albert Mends <mendsalbert@gmail.com>
2024-03-17 11:43:04 +0000

    cleaning up the dashboard

--- src/component/widget/TotalWidget.jsx
+++ src/component/widget/TotalWidget.jsx
@@ -10,7 +10,7 @@ function TotalWidget() {
           totalEarnImg={totalEarn}
           memberImg={memberImg}
           title="Current AQI"
-          amount="7,245.00"
+          amount="250.00"
           groth="+ 3.5%"
           id="totalEarn"
         />
