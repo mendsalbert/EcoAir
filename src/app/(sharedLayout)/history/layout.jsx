b6371234dab9730a092383b9d22c0f71ef5b0dc2
Albert Mends <mendsalbert@gmail.com>
2024-03-17 09:52:15 +0000

    update layout.jsx and page.js

--- src/app/(sharedLayout)/history/layout.jsx
+++ src/app/(sharedLayout)/history/layout.jsx
@@ -4,6 +4,7 @@ function AnalyticsLayout({ children }) {
   return (
     <main className="w-full px-6 pb-6 pt-[100px] sm:pt-[156px] xl:px-[48px] xl:pb-[48px] dark:bg-darkblack-700">
       <div className="2xl:flex 2xl:space-x-[48px]">{children}</div>
+      <div>hello world i added something</div>
     </main>
   );
 }
--- src/app/(sharedLayout)/page.js
+++ src/app/(sharedLayout)/page.js
@@ -9,7 +9,7 @@ import Efficiency from "@/component/revenueFlow/Efficiency";
 function Home() {
   return (
     <main className="w-full px-6 pb-6 pt-[100px] sm:pt-[156px] xl:px-12 xl:pb-12">
-      {/* write your code here */}
+      {/* write your code hereff */}
       <div className="2xl:flex 2xl:space-x-[48px]">
         <section className="mb-6 2xl:mb-0 2xl:flex-1">
           <TotalWidget />
