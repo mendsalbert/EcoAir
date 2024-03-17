7d7e9b6670a90ec66a40462e207eeff89d91c82f
Albert Mends <mendsalbert@gmail.com>
2024-03-17 15:33:22 +0000

    cleaning up the dashboard

--- src/app/(sharedLayout)/statistics/page.jsx
+++ src/app/(sharedLayout)/statistics/page.jsx
@@ -24,10 +24,6 @@ function Statistics() {
 
         <ListTab />
       </section>
-      <section className="flex w-full flex-col space-x-0 lg:flex-row lg:space-x-6 2xl:w-[400px] 2xl:flex-col 2xl:space-x-0">
-        <Wallet />
-        <Calender />
-      </section>
     </>
   );
 }
