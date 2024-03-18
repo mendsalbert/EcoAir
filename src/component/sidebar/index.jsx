eec101b587cc7af4d19eb843f7c0fbb4bfa3028c
Albert Mends <mendsalbert@gmail.com>
2024-03-18 07:33:08 +0000

    cleaning up the dashboard

--- src/component/sidebar/index.jsx
+++ src/component/sidebar/index.jsx
@@ -104,6 +104,42 @@ function Sidebar({ handleActive }) {
                   {/* </a> */}
                 </li>
               </Link>
+              <li className="item py-[11px] text-bgray-900 dark:text-white">
+                <Link href="/statistics">
+                  <div className="flex items-center justify-between">
+                    <div className="flex items-center space-x-2.5">
+                      <span className="item-ico">
+                        <svg
+                          width="20"
+                          height="20"
+                          viewBox="0 0 20 20"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M0 4C0 1.79086 1.79086 0 4 0H16C18.2091 0 20 1.79086 20 4V16C20 18.2091 18.2091 20 16 20H4C1.79086 20 0 18.2091 0 16V4Z"
+                            fill="#1A202C"
+                            className="path-1"
+                          />
+                          <path
+                            d="M14 9C12.8954 9 12 9.89543 12 11L12 13C12 14.1046 12.8954 15 14 15C15.1046 15 16 14.1046 16 13V11C16 9.89543 15.1046 9 14 9Z"
+                            fill="#22C55E"
+                            className="path-2"
+                          />
+                          <path
+                            d="M6 5C4.89543 5 4 5.89543 4 7L4 13C4 14.1046 4.89543 15 6 15C7.10457 15 8 14.1046 8 13L8 7C8 5.89543 7.10457 5 6 5Z"
+                            fill="#22C55E"
+                            className="path-2"
+                          />
+                        </svg>
+                      </span>
+                      <span className="item-text text-lg font-medium leading-none">
+                        Analytics
+                      </span>
+                    </div>
+                  </div>
+                </Link>
+              </li>
 
               <li className="item py-[11px] text-bgray-900 dark:text-white">
                 <Link href="/statistics">
