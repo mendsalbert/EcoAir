484938baea96229f9e042f0e21182ee9593cf825
Albert Mends <mendsalbert@gmail.com>
2024-03-18 07:54:39 +0000

    cleaning up the dashboard

--- src/app/(sharedLayout)/analytics/page.jsx
+++ src/app/(sharedLayout)/analytics/page.jsx
@@ -7,17 +7,7 @@ import LocationV2 from "@/component/location";
 import Efficiency from "@/component/revenueFlow/Efficiency";
 
 function Analytics() {
-  return (
     <>
-      {/* <section className="2xl:w-70 w-full 2xl:mb-0 mb-6">
-        <SummaryV3 />
-        <ListTab />
-      </section>
-      <section className="2xl:flex-1 w-full">
-        <EfficiencyV2 />
-        <TeamChat />
-      </section> */}
-
       <section className="2xl:flex-1 2xl:mb-0 mb-6">
         <div className="w-full mb-[24px]  ">
           <LocationV2 />
