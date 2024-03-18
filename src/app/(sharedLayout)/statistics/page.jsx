70f43625cd2790fbbc7b9c8c88e2e5c598ad7a50
Albert Mends <mendsalbert@gmail.com>
2024-03-18 18:54:22 +0000

    cleaning up the dashboard

--- src/app/(sharedLayout)/statistics/page.jsx
+++ src/app/(sharedLayout)/statistics/page.jsx
@@ -8,12 +8,13 @@ import TaskSummary from "@/component/summary/TaskSummary";
 import SummaryV3 from "@/component/summary/SummaryV3";
 import EfficiencyV2 from "@/component/revenueFlow/EfficiencyV2";
 import TeamChat from "@/component/teamChat";
+import SummaryHis from "@/component/summary/SummaryHis";
 
 function Statistics() {
   return (
     <>
       <div className="w-full mb-[24px] xl:flex xl:space-x-[24px]">
-        <SummaryV2 />
+        <SummaryHis />
         <Efficiency height="h-[180px]" />
       </div>
       <section className="2xl:w-70 w-full 2xl:mb-0 mb-6">
