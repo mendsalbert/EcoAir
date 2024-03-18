1462b326d096a3018dddfe9035d9756f1ddf1e9c
Albert Mends <mendsalbert@gmail.com>
2024-03-18 08:23:15 +0000

    cleaning up the dashboard

---
+++ src/component/chart/Histogram.jsx
@@ -0,0 +1,51 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler
+);
+
+function Histogram({ options, data }) {
+  // Adjust the options for the histogram
+  const histogramOptions = {
+    ...options,
+    scales: {
+      x: {
+        ...options.scales.x,
+        // Assuming 'options' already has a 'scales' property
+        barThickness: "flex", // This can be a number or 'flex'
+        maxBarThickness: 1, // This sets the maximum bar thickness
+        categoryPercentage: 1.0,
+        barPercentage: 1.0,
+      },
+      y: {
+        ...options.scales.y,
+        // Assuming 'options' already has a 'y' property
+        beginAtZero: true,
+      },
+    },
+    plugins: {
+      legend: {
+        display: false, // Typically histograms don't have a legend
+      },
+    },
+  };
+
+  return <Bar options={histogramOptions} data={data} />;
+}
+
+export default Histogram;
