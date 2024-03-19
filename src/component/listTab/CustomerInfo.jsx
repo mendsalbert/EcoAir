be58072bccd72e6939ae0ec80c08ed83c99a5614
Albert Mends <mendsalbert@gmail.com>
2024-03-19 12:31:49 +0000

    cleaning up the dashboard

--- src/component/listTab/CustomerInfo.jsx
+++ src/component/listTab/CustomerInfo.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import ProtoTypes from "prop-types";
 
-function CustomerInfo({ img, name, email, location, spent }) {
+function CustomerInfo({ img, name, email, location, spent, overall }) {
   return (
     <tr className="border-b border-bgray-300 dark:border-darkblack-400">
       <td className="">
@@ -45,39 +45,9 @@ function CustomerInfo({ img, name, email, location, spent }) {
         </p>
       </td>
       <td className="px-6 py-5 xl:px-0">
-        <div className="flex justify-center">
-          <button aria-label="none" type="button">
-            <svg
-              width="18"
-              height="4"
-              viewBox="0 0 18 4"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M8 2.00024C8 2.55253 8.44772 3.00024 9 3.00024C9.55228 3.00024 10 2.55253 10 2.00024C10 1.44796 9.55228 1.00024 9 1.00024C8.44772 1.00024 8 1.44796 8 2.00024Z"
-                stroke="#A0AEC0"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M1 2.00024C1 2.55253 1.44772 3.00024 2 3.00024C2.55228 3.00024 3 2.55253 3 2.00024C3 1.44796 2.55228 1.00024 2 1.00024C1.44772 1.00024 1 1.44796 1 2.00024Z"
-                stroke="#A0AEC0"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M15 2.00024C15 2.55253 15.4477 3.00024 16 3.00024C16.5523 3.00024 17 2.55253 17 2.00024C17 1.44796 16.5523 1.00024 16 1.00024C15.4477 1.00024 15 1.44796 15 2.00024Z"
-                stroke="#A0AEC0"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-          </button>
-        </div>
+        <p className="text-base font-semibold text-bgray-900 dark:text-white">
+          ${overall}
+        </p>
       </td>
     </tr>
   );
