44c051a03f5067c2b266ef042ed03fe4c8871d88
Albert Mends <mendsalbert@gmail.com>
2024-03-19 09:56:13 +0000

    cleaning up the dashboard

--- src/component/sidebar/SidebarV2.jsx
+++ src/component/sidebar/SidebarV2.jsx
@@ -56,51 +56,7 @@ function SidebarV2() {
                       </span>
                     </Link>
                   </li>
-                  <li className="item px-[43px] py-[11px]">
-                    <Link href="/transaction">
-                      <span className="item-ico">
-                        <svg
-                          width="18"
-                          height="20"
-                          viewBox="0 0 18 20"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            d="M18 16V6C18 3.79086 16.2091 2 14 2H4C1.79086 2 0 3.79086 0 6V16C0 18.2091 1.79086 20 4 20H14C16.2091 20 18 18.2091 18 16Z"
-                            fill="#1A202C"
-                            className="path-1"
-                          />
-                          <path
-                            fillRule="evenodd"
-                            clipRule="evenodd"
-                            d="M4.25 8C4.25 7.58579 4.58579 7.25 5 7.25H13C13.4142 7.25 13.75 7.58579 13.75 8C13.75 8.41421 13.4142 8.75 13 8.75H5C4.58579 8.75 4.25 8.41421 4.25 8Z"
-                            fill="#22C55E"
-                            className="path-2"
-                          />
-                          <path
-                            fillRule="evenodd"
-                            clipRule="evenodd"
-                            d="M4.25 12C4.25 11.5858 4.58579 11.25 5 11.25H13C13.4142 11.25 13.75 11.5858 13.75 12C13.75 12.4142 13.4142 12.75 13 12.75H5C4.58579 12.75 4.25 12.4142 4.25 12Z"
-                            fill="#22C55E"
-                            className="path-2"
-                          />
-                          <path
-                            fillRule="evenodd"
-                            clipRule="evenodd"
-                            d="M4.25 16C4.25 15.5858 4.58579 15.25 5 15.25H9C9.41421 15.25 9.75 15.5858 9.75 16C9.75 16.4142 9.41421 16.75 9 16.75H5C4.58579 16.75 4.25 16.4142 4.25 16Z"
-                            fill="#22C55E"
-                            className="path-2"
-                          />
-                          <path
-                            d="M11 0H7C5.89543 0 5 0.895431 5 2C5 3.10457 5.89543 4 7 4H11C12.1046 4 13 3.10457 13 2C13 0.895431 12.1046 0 11 0Z"
-                            fill="#22C55E"
-                            className="path-2"
-                          />
-                        </svg>
-                      </span>
-                    </Link>
-                  </li>
+
                   <li className="item px-[43px] py-[11px]">
                     <Link href="/statistics">
                       <span className="item-ico">
