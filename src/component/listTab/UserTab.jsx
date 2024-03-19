6f919c18e23f6995843d7746596f0e915a1d3e85
Albert Mends <mendsalbert@gmail.com>
2024-03-19 12:27:13 +0000

    cleaning up the dashboard

--- src/component/listTab/UserTab.jsx
+++ src/component/listTab/UserTab.jsx
@@ -195,6 +195,51 @@ function UserTab({ pageSize }) {
                 </span>
               </div>
             </td>
+            <td className="w-[165px] px-6 py-5 xl:px-0">
+              <div className="flex w-full items-center space-x-2.5">
+                <span className="text-base font-medium text-bgray-600 dark:text-bgray-50">
+                  Overall Air Quality
+                </span>
+                <span>
+                  <svg
+                    width="14"
+                    height="15"
+                    viewBox="0 0 14 15"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M10.332 1.31567V13.3157"
+                      stroke="#718096"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                    <path
+                      d="M5.66602 11.3157L3.66602 13.3157L1.66602 11.3157"
+                      stroke="#718096"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                    <path
+                      d="M3.66602 13.3157V1.31567"
+                      stroke="#718096"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                    <path
+                      d="M12.332 3.31567L10.332 1.31567L8.33203 3.31567"
+                      stroke="#718096"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </span>
+              </div>
+            </td>
           </tr>
           {users?.map((user, index) =>
             pageSize
