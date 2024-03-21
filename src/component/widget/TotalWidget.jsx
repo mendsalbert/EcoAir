973bc1f7a9729eb6af215b388c1f9b1a95b0612e
Albert Mends <mendsalbert@gmail.com>
2024-03-21 22:33:35 +0000

    cleaning up the dashboard

--- src/component/widget/TotalWidget.jsx
+++ src/component/widget/TotalWidget.jsx
@@ -30,7 +30,7 @@ function TotalWidget() {
 
     fetchLocations();
   }, []);
-  console.log(locations);
+  console.log("locations");
   return (
     <div className="mb-[24px] w-full">
       <div className="grid grid-cols-1 gap-[24px] lg:grid-cols-3">
