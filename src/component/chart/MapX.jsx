e44a19a012467ff4b4463662f0c9342e79e5f745
Albert Mends <mendsalbert@gmail.com>
2024-03-24 07:23:16 +0000

    cleaning up the dashboard

--- src/component/chart/MapX.jsx
+++ src/component/chart/MapX.jsx
@@ -101,10 +101,19 @@ function Map() {
   };
 
   return (
-    <div
-      id="world-map"
-      className="w-full h-screen rounded-lg border border-[#E2E8F0] overflow-hidden"
-    ></div>
+    // <div
+    //   id="world-map"
+    //   className="w-full h-screen rounded-lg border border-[#E2E8F0] overflow-hidden"
+    // ></div>
+    <div style={{ height: "100vh", width: "100%" }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "" }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+        <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
+      </GoogleMapReact>
+    </div>
   );
 }
 
