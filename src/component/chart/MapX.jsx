92fe74e52501e5a6758a2f33ff13d61bb95f6ee5
Albert Mends <mendsalbert@gmail.com>
2024-03-24 11:52:11 +0000

    cleaning up the dashboard

--- src/component/chart/MapX.jsx
+++ src/component/chart/MapX.jsx
@@ -36,11 +36,22 @@ function Map() {
   };
 
   const initMap = () => {
-    const markers = locations.map((location, index) => ({
-      name: location.name,
-      coords: [location.coordinates.latitude, location.coordinates.longitude],
-      index,
-    }));
+    // const markers = locations.map((location, index) => ({
+    //   name: location.name,
+    //   coords: [location.coordinates.latitude, location.coordinates.longitude],
+    //   index,
+    // }));
+    markers: [
+      { name: "Egypt", coords: [26.8206, 30.8025] },
+      { name: "United Kingdom", coords: [55.3781, 3.436] },
+      {
+        name: "United States",
+        coords: [37.0902, -95.7129],
+        // Add style for this particular marker
+        // Keep in mind `style` object is merged with `markerStyle.initial`
+        style: { fill: "red" },
+      },
+    ];
 
     console.log("====makers====", markers[0]);
     const newMap = new jsVectorMap({
