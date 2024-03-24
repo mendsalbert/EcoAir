9db613a43fb67f405ff7bc6c5c49a32048279587
Albert Mends <mendsalbert@gmail.com>
2024-03-24 12:05:10 +0000

    cleaning up the dashboard

--- src/component/chart/MapX.jsx
+++ src/component/chart/MapX.jsx
@@ -44,7 +44,7 @@ function Map() {
     console.log("====makers====", markers[0]);
 
     const newMap = new jsVectorMap({
-      map: "world",
+      map: "world_merc",
       selector: "#world-map",
       markers: [
         { name: "Egypt", coords: [26.8206, 30.8025] },
