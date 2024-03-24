0be85c379189ab63d9128cae92b8fc2e64f1f3b8
Albert Mends <mendsalbert@gmail.com>
2024-03-24 11:53:46 +0000

    cleaning up the dashboard

--- src/component/chart/MapX.jsx
+++ src/component/chart/MapX.jsx
@@ -35,30 +35,33 @@ function Map() {
     }
   };
 
+ 
   const initMap = () => {
     // const markers = locations.map((location, index) => ({
     //   name: location.name,
     //   coords: [location.coordinates.latitude, location.coordinates.longitude],
     //   index,
     // }));
-    markers: [
-      { name: "Egypt", coords: [26.8206, 30.8025] },
-      { name: "United Kingdom", coords: [55.3781, 3.436] },
-      {
-        name: "United States",
-        coords: [37.0902, -95.7129],
-        // Add style for this particular marker
-        // Keep in mind `style` object is merged with `markerStyle.initial`
-        style: { fill: "red" },
-      },
-    ];
+  
 
     console.log("====makers====", markers[0]);
     const newMap = new jsVectorMap({
       map: "world",
       selector: "#world-map",
       //   markersSelectableOne: true,
-      markers: markers, // Use the markers from state
+    //   markers: markers, 
+    markers: [
+        { name: "Egypt", coords: [26.8206, 30.8025] },
+        { name: "United Kingdom", coords: [55.3781, 3.436] },
+        {
+          name: "United States",
+          coords: [37.0902, -95.7129],
+          // Add style for this particular marker
+          // Keep in mind `style` object is merged with `markerStyle.initial`
+          style: { fill: "red" },
+        },
+      ];
+      // Use the markers from state
       //   markerStyle: {
       //     initial: {
       //       fill: "#22C55E",
