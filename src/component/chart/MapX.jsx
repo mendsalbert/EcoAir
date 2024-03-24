76624127c0dabc03f6fe0bac386fa5ae06282481
Albert Mends <mendsalbert@gmail.com>
2024-03-24 11:40:44 +0000

    cleaning up the dashboard

--- src/component/chart/MapX.jsx
+++ src/component/chart/MapX.jsx
@@ -45,7 +45,7 @@ function Map() {
     const newMap = new jsVectorMap({
       map: "world",
       selector: "#world-map",
-      markersSelectable: true, // The markers are selectable
+      //   markersSelectable: true, // The markers are selectable
       markers: markers, // Use the markers from state
       markerStyle: {
         initial: {
