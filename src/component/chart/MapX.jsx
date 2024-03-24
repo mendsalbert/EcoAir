b60b76de61cd554df6e6a48da256a31d80ea9b3e
Albert Mends <mendsalbert@gmail.com>
2024-03-24 11:10:51 +0000

    cleaning up the dashboard

--- src/component/chart/MapX.jsx
+++ src/component/chart/MapX.jsx
@@ -56,7 +56,7 @@ function Map() {
       zoomButtons: true,
       zoomOnScroll: true,
       zoomOnScrollSpeed: 3,
-      zoomMax: 24,
+      zoomMax: 36,
       zoomMin: 1,
       zoomAnimate: true,
       showTooltip: true,
