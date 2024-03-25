0be71b4e9720618864de4038848c6fa55b6156ab
Albert Mends <mendsalbert@gmail.com>
2024-03-25 01:52:51 +0000

    cleaning up the dashboard

--- src/app/(sharedLayout)/map/layout.jsx
+++ src/app/(sharedLayout)/map/layout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function MapLayout({ children }) {
   return (
-    <main className="w-full xl:px-12 px-6 pb-6 xl:pb-12 sm:pt-[156px] pt-[100px]">
+    <main>
       <div className="2xl:flex 2xl:space-x-[48px]">{children}</div>
     </main>
   );
