8b2989dc5c5e6c58cefd59d5b14dedb45897c15f
Albert Mends <mendsalbert@gmail.com>
2024-03-25 16:52:48 +0000

    cleaning up the dashboard

--- src/component/chart/MapX.jsx
+++ src/component/chart/MapX.jsx
@@ -175,7 +175,8 @@ export default function App() {
   return (
     <>
       <div
-      //   className="w-screen h-screen rounded-lg border border-[#E2E8F0] overflow-hidden"
+        className="h-screen"
+        //   className="w-screen h-screen rounded-lg border border-[#E2E8F0] overflow-hidden"
       >
         <Map
           initialViewState={{
