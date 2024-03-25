08b2acf768618a7a92585609ae685ad3db87333b
Albert Mends <mendsalbert@gmail.com>
2024-03-25 11:04:25 +0000

    cleaning up the dashboard

--- src/component/chart/MapX.jsx
+++ src/component/chart/MapX.jsx
@@ -224,11 +224,11 @@ export default function App() {
           mapStyle="mapbox://styles/mapbox/dark-v9"
           mapboxAccessToken={MAPBOX_TOKEN}
         >
-          {/* {data && (
+          {data && (
             <Source type="geojson" data={data}>
               <Layer {...heatmapLayer} />
             </Source>
-          )} */}
+          )}
         </MapGL>
         {/* <ControlPanel
           startTime={timeRange[0]}
